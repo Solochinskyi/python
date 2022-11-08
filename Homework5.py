2df5033836ed8f711f8e87767a7c276d9b4f4c18
Solochinskyi <Solochinskyi@gmail.com>
2022-11-08 21:06:18 +0200

    Homework5

---
+++ Homework5.py
@@ -0,0 +1,46 @@
+#task 1
+#slovo = str(input('Enter value a: '))
+#x = len(slovo)
+#i = 0
+#x = x - 1
+#k = 0
+#while x - i >= i:
+ #   if slovo[x - i] == slovo[i]:
+#        i += 1
+#    else:
+ #       k = 1
+ #       break
+#if k == 1:
+#  print("no")
+#else:
+#  print("yes")
+#-----------------------------------------------------------------------------------
+#Task 2
+#slova = str(input('Enter sentse: '))
+#word = str(input('Enter some word: '))
+
+#if slova.find(f'{word}') != -1:
+#    print('Yes')
+#else:
+#    print('no')
+#------------------------------------------------------------------------------------------------
+#Task 3
+#slova = str(input('Enter sentse: '))
+
+#if slova.startswith('abc'):
+# print(slova.replace("abc", "www", 1))
+#else:
+# print(f'{slova}' + 'zzz')
+#------------------------------------------------------------------------------------------------
+#Task 4
+#slova = str(input('Enter sentse: '))
+#s1="".join(i for i in slova if i.isdigit()!= 1)
+#print (s1)
+#-----------------------------------------------------------------------------------------------
+#Task 5
+#mail = str(input('Enter sentse: '))
+
+#if mail.find('@') != -1 and mail.find('_') != -1:
+#    print('Валідація успішна')
+#else:
+#    print('Попробуйте знову')
